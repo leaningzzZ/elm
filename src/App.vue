69e3a9970faa8384953f932eec906f7619aed988
liuzhe <1040583465@qq.com>
2019-06-03 12:48:37 +0800

    前端开发环境初始化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <div id="app">
+  <div class="container">
+    <home-page></home-page>
   </div>
 </template>
-
+<script>
+import homePage from "./components/home/home"
+export default {
+  components:{
+    homePage,
+  }
+}
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container{
+  height: 100%;
 }
 </style>
---
+++ src/components/home/home.vue
@@ -0,0 +1,55 @@
+<template>
+    <el-container class="content">
+      <el-header height="200px">
+          <img class="header-left" style="width:130px" src="//shadow.elemecdn.com/faas/h5/static/logo.ba876fd.png" alt="">
+          <!-- <span class="header-right">
+              <a href="#">登陆</a>
+              <span>|</span>
+              <a href="#">注册</a>
+          </span> -->
+      </el-header>
+      <el-main>
+            <div class="main-body">
+              <img style="height:100%;" src="//shadow.elemecdn.com/faas/h5/static/logo.ba876fd.png" alt="eleme">
+            </div>
+            <div class="main-body">
+                <el-button type="primary" size="small">主要按钮</el-button> 
+                <el-button type="primary" size="small">主要按钮</el-button>
+            </div>
+      </el-main>
+      <el-footer>assda</el-footer>
+    </el-container>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style scoped>
+a{
+    text-decoration: none;
+    color:#fff
+}
+.container{
+    height: 100%;
+}
+.content{
+    padding-top: 40px;
+        height: 100%;
+}
+.header-left{
+    float: left;
+    vertical-align: middle;
+}
+.header-right{
+    color: #fff;
+    margin-top: 30px;
+}
+.main-body{
+    height: 120px;
+    text-align: center;
+}
+.main-body span{
+    color: #fff;
+}
+</style>
