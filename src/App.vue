fbab555c9dc21e08e804e89bab2a86fa27d3de6b
liuzhe <1040583465@qq.com>
2019-06-04 10:23:33 +0800

    homePage页面初始化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="container">
-    <login-page></login-page>
+    <login-page v-if="!isLogined"></login-page>
+    <home-page v-else></home-page>
   </div>
 </template>
 <script>
 import loginPage from "./components/login/login"
+import homePage from "./components/home/home"
 export default {
   components:{
     loginPage,
+    homePage
+  },
+  data(){
+    return {
+      isLogined:true
+    }
   }
 }
 </script>
---
+++ src/components/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <tab></tab>
+  </div>
+</template>
+<script>
+import tab from "../tab/tab"
+export default {
+  components:{
+    tab
+  }
+}
+</script>
+<style scoped>
+
+</style>
