b24b82caf8ae55980e4c6b906fdaadc9cfba3410
liuzhe <1040583465@qq.com>
2019-06-10 23:03:56 +0800

    主页接口联调完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import { api } from "./api/api";
+// import jsonp from "vue-jsonp"
+// Vue.use(jsonp)
+Vue.prototype.$api=api
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 new Vue({
