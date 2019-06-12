365b0b5b79c7b0682f1bb2db9e2aa15e5072a91a
actt0422 <actt422@outlook.com>
2019-06-12 23:17:49 +0800

    temp

--- src/components/home/rstDetail.vue
+++ src/components/home/rstDetail.vue
@@ -26,30 +26,55 @@
             <p>起送价</p>
             <p>{{rstData.startPrice}}元</p>
           </span>
+          <div class="collect" @click="collect">
+            <i v-if="!rstData.isCollect" class="el-icon-star-off"></i>
+            <i v-else class="el-icon-star-on"></i>
+            <div v-if="!rstData.isCollect">收藏</div>
+            <div v-else>取消收藏</div>
+          </div>
         </div>
       </div>
     </el-header>
+    <div class="nav">
+      <div class="navContent">
+        <span @click="selectFood" :style="foodStyle">全部商品</span>
+        <span @click="selectRate" :style="rateStyle">评价</span>
+      </div>
+    </div>
+
     <el-main style="height:1000px;">
       <div class="food">
-        <p>全部商品</p>
-        <div class="foodBlock" v-for="item in foodData" :key="item.id">
-          <img :src="item.logo">
-          <div class="foodMain">
-            <span class="foodBlockName">{{item.name}}</span>
-            <el-rate
-              v-model="item.star"
-              style="display:inline-block;float:left;line-height:20px;"
-              disabled
-            ></el-rate>
-            <span class="foodBlockSale">月售{{item.sale}}份</span>
-            <span class="foodBlockPrice">￥{{item.price}}</span>
+        <div class="foodContent" v-if="select == 'food'">
+          <p>全部商品</p>
+          <div class="foodBlock" v-for="item in foodData" :key="item.id">
+            <img :src="item.logo">
+            <div class="foodMain">
+              <span class="foodBlockName">{{item.name}}</span>
+              <el-rate
+                v-model="item.star"
+                style="display:inline-block;float:left;line-height:20px;"
+                disabled
+              ></el-rate>
+              <span class="foodBlockSale">月售{{item.sale}}份</span>
+              <span class="foodBlockPrice">￥{{item.price}}</span>
+            </div>
+            <el-button
+              type="primary"
+              round
+              size="mini"
+              style="position:absolute;bottom:10px;right:10px;"
+            >加入购物车</el-button>
           </div>
-          <el-button
-            type="primary"
-            round
-            size="mini"
-            style="position:absolute;bottom:10px;right:10px;"
-          >加入购物车</el-button>
+        </div>
+        <div v-if="select == 'rate'" class="rate">
+          <p>评价</p>
+          <el-table :data="tableData" style="width: 850px">
+            <el-table-column prop="name" label="用户" width="140"></el-table-column>
+            <el-table-column prop="address" label="评价"></el-table-column>
+            <div v-for="item in tableData.foodRate" :key="item.id">
+              <span>{{item.name}}</span>
+            </div>
+          </el-table>
         </div>
         <div class="message">
           <div class="messageTitle">
@@ -74,6 +99,51 @@
 export default {
   data() {
     return {
+      tableData: [
+        {
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          foodRate:[
+            {
+              name:'手工馒头',
+              rate:5,
+            },
+            {
+              name:'手工馒头',
+              rate:5,
+            },
+            {
+              name:'手工馒头',
+              rate:5,
+            },
+            {
+              name:'手工馒头',
+              rate:5,
+            },
+          ]
+        },
+        {
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄"
+        },
+        {
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄"
+        },
+        {
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄"
+        }
+      ],
+      select: "food",
+      foodStyle: {
+        color: "#0089DC",
+        borderBottom: "2px soild #0089DC"
+      },
+      rateStyle: {
+        color: "#333333",
+        borderBottom: "none"
+      },
       rstData: {
         logo:
           "//fuss10.elemecdn.com/e/63/23054dfca5d9e008fc0972a3306c9jpeg.jpeg?imageMogr2/thumbnail/95x95/format/webp/quality/85",
@@ -81,7 +151,8 @@ export default {
         star: "5",
         time: 45,
         price: 0.1,
-        startPrice: 15
+        startPrice: 15,
+        isCollect: false
       },
       foodData: [
         {
@@ -138,6 +209,24 @@ export default {
     initData() {
       this.storeId = this.$route.params.rstId;
       console.log(this.storeId);
+    },
+    collect() {
+      this.rstData.isCollect = !this.rstData.isCollect;
+    },
+    selectFood() {
+      this.select = "food";
+
+      this.foodStyle.color = "#0089DC";
+      this.foodStyle.borderBottom = "2px soild #0089DC";
+      this.rateStyle.color = "#333333";
+      this.rateStyle.borderBottom = "none";
+    },
+    selectRate() {
+      this.select = "rate";
+      this.foodStyle.color = "#333333";
+      this.foodStyle.borderBottom = "none";
+      this.rateStyle.color = "#0089DC";
+      this.rateStyle.borderBottom = "2px soild #0089DC";
     }
   }
 };
@@ -189,6 +278,7 @@ p {
   vertical-align: middle;
 }
 .delivery {
+  position: relative;
   float: left;
   width: 700px;
   height: 142px;
@@ -199,6 +289,21 @@ p {
   color: #fff;
   font-size: 18px;
   text-align: center;
+}
+.collect {
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 10px 5px;
+  width: 80px;
+  text-align: center;
+  box-sizing: border-box;
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 0 0 5px 5px;
+}
+.collect div {
+  color: #eeeeee;
+  font-size: 14px;
 }
 .deliveryTime {
   float: right;
@@ -225,6 +330,24 @@ p {
 .deliveryStartPrice :first-child {
   font-size: 14px;
   margin-bottom: 12px;
+}
+.nav {
+  height: 57px;
+  line-height: 57px;
+  background-color: #fff;
+}
+.navContent {
+  width: 1260px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.navContent span {
+  width: 102px;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+  border-bottom: none;
 }
 .food {
   position: relative;
